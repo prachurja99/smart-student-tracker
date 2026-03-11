a2837089f3cf7a7ca3ab58bd1c3dd88c73ded59e
prachurja99 <bhattprachu180@gmail.com>
2026-03-11 21:03:56 +0600

    fix: replace all hardcoded ML service URLs

--- backend/controllers/gradeController.js
+++ backend/controllers/gradeController.js
@@ -203,7 +203,7 @@ const getMyMLAnalysis = async (req, res) => {
       examDate: g.examDate,
     }));
 
-    const mlResponse = await axios.post('http://127.0.0.1:5001/analyze', {
+    const mlResponse = await axios.post(`${process.env.ML_SERVICE_URL || 'http://127.0.0.1:5001'}/analyze`, {
       grades: gradeData,
     });
 
